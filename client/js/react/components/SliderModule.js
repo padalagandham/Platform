0dd067047181ec9de3844392ed66a0e7003e6a01
Grishma Marathi <grishma.marathi@macys.com>
2015-06-17 18:45:16 +0530

    commit for sliding slides automatically in corousel

--- client/js/react/components/SliderModule.js
+++ client/js/react/components/SliderModule.js
@@ -1,39 +1,50 @@
 define(['react','templates/components/ImageModule','templates/components/TextModule',], function( React,ImageModule,TextModule) {
 
 	var SliderModule=React.createClass({
+
+
+		componentDidMount: function() {
+		 	console.log("componentDidMount");
+  			if (this.isMounted()) {
+  				var idd="#"+this.props.data.sliderId;
+  				$(idd).carousel({
+      				interval: 2500
+    			})
+  			}
+  		},
+  		
 		render:function(){
 
 				console.log("dsfs",this.props.data)
 					var a=[],b=[];
 
- this.props.data.sliderObj.forEach(function(sliderObj){
- 		var dataTargetAttr="#"+sliderObj.sliderId;
-		a.push(
-				<li data-target={dataTargetAttr} data-slide-to={sliderObj.slideTo} className={sliderObj.hasActive}></li>
-				);
-		});
+			 this.props.data.sliderObj.forEach(function(sliderObj){
+			 		var dataTargetAttr="#"+sliderObj.sliderId;
+					a.push(
+							<li data-target={dataTargetAttr} data-slide-to={sliderObj.slideTo} className={sliderObj.hasActive}></li>
+							);
+					});
 
-   this.props.data.sliderObj.forEach(function(sliderObj){
-   
-		   	var classString="item " + sliderObj.hasActive;
-		     if(sliderObj.media.type==="image"){
-		     b.push(
-		     	 <div className={classString}>
-		      <ImageModule imgObj={sliderObj}/></div>
-		      )
-		     }
+			   this.props.data.sliderObj.forEach(function(sliderObj){
+			   
+					   	var classString="item " + sliderObj.hasActive;
+					     if(sliderObj.media.type==="image"){
+					     b.push(
+					     	 <div className={classString}>
+					      <ImageModule imgObj={sliderObj}/></div>
+					      )
+					     }
 
-		      if(sliderObj.media.type==="text"){
-		     b.push(
-		     	 <div className={classString}>
-		      <TextModule txtObj={sliderObj}/></div>
-		      )
-		     }
-		    
-		    
-   });
+					      if(sliderObj.media.type==="text"){
+					     b.push(
+					     	 <div className={classString}>
+					      <TextModule txtObj={sliderObj}/></div>
+					      )
+					     }
+					    
+					    
+			   });
 			return( 
-
 					<div id={this.props.data.sliderId} className="carousel slide col-md-12" data-ride="carousel">
 						<ol className="carousel-indicators">
 							{a}
